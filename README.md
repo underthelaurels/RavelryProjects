902fe7c44fe01994544d7ad9d8ad38804d3ac5fa
Laurel Romoser <laurelr@vt.edu>
2021-08-12 10:32:01 -0600

    added liscence, requirements, and some more printed stash info

--- README.md
+++ README.md
@@ -1,28 +1,25 @@
 # RavelryProjects
 Small projects using the Ravelry API to improve personal use of Ravelry as well as knitting knowledge.
-Using python 3
-
-Current dependencies - python requests. ("python3 -m pip install requests" to install!)
 
 <br />
 
 ## Stash Organizer [WIP]
-Data visualization of your yarn stash. Sortable and filterable.
-Currently collects data from Ravelry and parses and prints out basic stash information. 
-You can also dump the stash information as stash_dump.json, weight_dump.json, and color_dump.json by changing "org = StashOrganizer(0)" in stashOrganizer.py to "org = StashOrganizer(1)". 
+Data visualization of your yarn stash. Sortable and filterable. <p>
+Currently collects data from Ravelry and parses and prints out basic stash information. <p>
+You can also dump the stash information into several files as stash_dump.json, weight_dump.json, and color_dump.json by changing "org = StashOrganizer(0)" in stashOrganizer.py to "org = StashOrganizer(1)". <p>
 I have added an example config file if you would like to try it out on your on your own machine! You can request Ravelry api tokens at https://www.ravelry.com/pro/developer
 
 <br />
 
-## Scrap Tagger [WIP]
+## Scrap Tagger [Not Started]
 Auto tags stash yarn as scraps if the leftovers from a project are deemed as scrap. Adds scrap yarn automatically to a selected scrap project in the queue.
 
 <br />
 
-## Project Data Visualization [WIP]
+## Project Data Visualization [Not Started]
 Some charts containing fun information about project data visualization
 
 <br />
 
-## Pattern Stitch and Hour Estimator [WIP]
+## Pattern Stitch and Hour Estimator [Not Started]
 Estimates the number of stitches in an unmodded project given the size made. Uses this to estimate the number of hours it would take to complete the project given a knitter's stitches per minute. 
--- stashOrganizer.py
+++ stashOrganizer.py
@@ -1,4 +1,5 @@
 import json
+from os import path
 from types import TracebackType
 import requests
 from requests.models import Response
@@ -7,76 +8,114 @@ class StashOrganizer:
 
     def __init__(self, info):
         # import the json and store username and password
-        with open('config.json') as json_file:
-            data = json.load(json_file)
+        with open('config.json') as jsonFile:
+            data = json.load(jsonFile)
             self.username = data['personal']['user']
             self.password = data['personal']['pass']
             self.person = data['person']
 
-        # fetch all the stash data once
+        # fetch all the stash data
         self.stash = self.fetchStash()
-        self.colors = self.fetchColorfams()
-        self.weights = self.fetchYarnWeights()
 
         if info:
+            self.colors = self.fetchColorfams()
+            self.weights = self.fetchYarnWeights()
             self.prettyDump('stash')
             self.prettyDump('colors')
             self.prettyDump('weights')
 
+    ###
+    ### Fetch functions for getting Ravelry data
+    ### 
 
     def fetchStash(self):
-        #Fetch personal stash data from Ravelry
-        url = 'https://api.ravelry.com/people/' + self.person + '/stash/list.json'
-        params = {'sort': 'weight'}
-        response = requests.get(url, auth=(self.username, self.password), params=params)
-        data = response.json()['stash']
+        # Only get stash from Ravelry if dump file doesn't exist
+        if (path.exists('stash_dump.json')):
+            with open('stash_dump.json') as jsonFile:
+                data = json.load(jsonFile)
+        else:
+            # Fetch personal stash data from Ravelry
+            url = 'https://api.ravelry.com/people/' + self.person + '/stash/list.json'
+            params = {'sort': 'weight'}
+            response = requests.get(url, auth=(self.username, self.password), params=params)
+            data = response.json()['stash']
         return data
 
     def fetchColorfams(self):
-        # Get the list of color families from Ravelry
-        url = 'https://api.ravelry.com/color_families.json'
-        response = requests.get(url, auth=(self.username, self.password))
-        data = response.json()['color_families']
+        # Only get colors from Ravelry if dump file doesn't exist
+        if (path.exists('color_dump.json')):
+            with open('color_dump.json') as jsonFile:
+                data = json.load(jsonFile)
+        else:
+            # Get the list of color families from Ravelry
+            url = 'https://api.ravelry.com/color_families.json'
+            response = requests.get(url, auth=(self.username, self.password))
+            data = response.json()['color_families']
         return data
     
     def fetchYarnWeights(self):
-        # Get the list of yarn weights from Ravelry
-        url = 'https://api.ravelry.com/yarn_weights.json'
-        response = requests.get(url, auth=(self.username, self.password))
-        data = response.json()['yarn_weights']
+        # Only get weights from Ravelry if dump file doesn't exist
+        if (path.exists('weight_dump.json')):
+            with open('weight_dump.json') as jsonFile:
+                data = json.load(jsonFile)
+        else:
+            # Get the list of yarn weights from Ravelry
+            url = 'https://api.ravelry.com/yarn_weights.json'
+            response = requests.get(url, auth=(self.username, self.password))
+            data = response.json()['yarn_weights']
         return data
 
+    ###
+    ### Functions to manipulate and display stash data
+    ###
 
     def printBasicStashInfo(self):
-        print("Total stash items: ", len(self.stash))
-        
-        # we countin now
+        # we countin
         inStashCount = 0
         usedCount = 0
         giftedCount = 0
 
         colorCounts = dict()
+        weightCounts = dict()
 
-        for yarn in self.stash:
-            if yarn['stash_status']['id'] == 1:
+        for stashItem in self.stash:
+            # counting yarn statuses
+            if stashItem['stash_status']['id'] == 1:
                 inStashCount += 1
-            elif yarn['stash_status']['id'] == 2:
+            elif stashItem['stash_status']['id'] == 2:
                 usedCount += 1
-            elif yarn['stash_status']['id'] == 4:
+            elif stashItem['stash_status']['id'] == 4:
                 giftedCount += 1
             
-            if yarn['color_family_name'] in colorCounts:
-                colorCounts[yarn['color_family_name']] += 1
-            elif yarn['color_family_name'] != None:
-                colorCounts[yarn['color_family_name']] = 1
+            # counting yarns of each color (only if currently in stash)
+            if stashItem['stash_status']['id'] == 1:
+                if stashItem['color_family_name'] in colorCounts:
+                    colorCounts[stashItem['color_family_name']] += 1
+                elif stashItem['color_family_name'] != None:
+                    colorCounts[stashItem['color_family_name']] = 1
 
-        # printing out the counts
-        print("In stash: ", inStashCount)
-        print("Used up: ", usedCount)
-        print("Gifted: ", giftedCount)
-        print("Stash Color Counts:") # Print the stash color counts from most to least frequent
+            # counting yarns of each weight (only if currently in stash)
+            if 'yarn' in stashItem and stashItem['stash_status']['id'] == 1:
+                if stashItem['yarn']['yarn_weight']['name'] in weightCounts:
+                    weightCounts[stashItem['yarn']['yarn_weight']['name']] += 1
+                else:
+                    weightCounts[stashItem['yarn']['yarn_weight']['name']] = 1
+
+        # print the stash status counts
+        print('Total stash items:', len(self.stash))
+        print('In stash:', inStashCount)
+        print('Used up:', usedCount)
+        print('Gifted:', giftedCount)
+
+        # print the stash color counts from most to least frequent
+        print('\nStash Colors: (in stash only)') 
         for color in sorted(colorCounts, key=colorCounts.get, reverse=True):
-            print("    ", color, ":", colorCounts[color])
+            print('    ', color, ':', colorCounts[color])
+        
+        # print the stash weight counts
+        print('\nStash Weights: (in stash only)')
+        for weight in weightCounts:
+            print('    ', weight, ':', weightCounts[weight])
 
 
     def prettyDump(self, type):
@@ -90,6 +129,10 @@ class StashOrganizer:
             with open('weight_dump.json', 'w') as writeFile:
                 json.dump(self.weights, writeFile, indent=4)
 
+
+###
+### Function calls
+###
 
 org = StashOrganizer(0)
 org.printBasicStashInfo()
